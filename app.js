c55b8e705fd0090cf346f7611d07fb02bc31d479
adedamola-toye <toye.adedamola@gmail.com>
2025-04-06 15:19:16 +0100

    add start to package.json

--- app.js
+++ app.js
@@ -34,7 +34,7 @@ const swaggerOptions = {
 
 app.use("/", caseRoutes);
 
-const PORT = process.env.PORT || 5001;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
 module.exports = app;
